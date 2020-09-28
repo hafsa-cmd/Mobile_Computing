84f4c2c8e15200fb9fa2841bb73e4ea1afed02e0
hafsa-cmd <bitf17a550@pucit.edu.pk>
2020-09-28 06:17:47 +0500

    Second Commit

--- Assign01/app/src/main/java/com/example/assign01/MainActivity.java
+++ Assign01/app/src/main/java/com/example/assign01/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.assign01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    View viewBg;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewBg = (View) findViewById(R.id.view);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewBg.setBackgroundColor(Color.parseColor("#df80ff"));
+            }
+        });
     }
 }
